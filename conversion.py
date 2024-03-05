a895b4032b9d8dcc1bece7a616bc64c526fe21bd
Charles <cvhees15@gmail.com>
2024-03-05 12:20:47 +0100

    Conversion sphérique cartésien

--- conversion.py
+++ conversion.py
@@ -1,11 +1,38 @@
-from math import sqrt, acos, sin, cos
-def polar(x, y, z):
+from math  import sqrt, acos, atan, sin, cos
+from numpy import pi, sign
+
+def polar(x, y, z) :
+    """
+    Transform cartesian coordinates into spherical coordinates
+    Input :
+    - x  (float)
+    - y  (float)
+    - z  (float)
+
+    Output :
+    - r     = sqrt(x**2 + y**2 + z**2)  (float)
+    - phi   = arccos(z/r)
+    - theta = 
+    """
     r = sqrt(x**2 + y**2 + z**2)
     phi = acos(z/r)
-    theta = y/abs(y)*x/sqrt(x**2 + y**2)
+    theta = acos(x/sqrt(x**2 + y**2))
+    theta = acos(x/sqrt(x**2 + y**2)) + pi*(1 - sign(x))/2
     return r, phi, theta
 
-def cartesian(r, phi, theta):
+def cartesian(r, phi, theta) :
+    """
+    Transform spherical coordinates into cartesian coordinates
+    Input :
+    - 0 <= r             (float)
+    - 0 <= phi   <= pi   (float)
+    - 0 <= theta <= 2pi  (float)
+
+    Output :
+    - x = r*sin(phi)*cos(theta)  (float)
+    - y = r*sin(phi)*sin(theta)  (float)
+    - z = r*cos(phi)             (float)
+    """
     x = r*sin(phi) * cos(theta)
     y = r*sin(phi) * sin(theta)
     z = r*cos(phi)
