ca0a7312b38316b4097ab69325c75be2db862e6d
hokkaydo <hokkaydo@users.noreply.github.com>
2024-03-05 09:40:26 +0100

    add conversion

---
+++ conversion.py
@@ -0,0 +1,12 @@
+from math import sqrt, acos, sin, cos
+def polar(x, y, z):
+    r = sqrt(x**2 + y**2 + z**2)
+    theta = acos(z/r)
+    phi = y/abs(y)*x/sqrt(x**2 + y**2)
+    return r, phi, theta
+
+def cartesian(r, phi, theta):
+    x = r*sin(phi) * cos(theta)
+    y = r*sin(phi) * sin(theta)
+    z = r*cos(phi)
+    return x, y, z
