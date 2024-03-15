91a78799f11260a2e55e453e9eb8cf268655ceb4
Charles <cvhees15@gmail.com>
2024-03-15 19:25:49 +0100

    cost functions

--- utilities.py
+++ utilities.py
@@ -5,13 +5,13 @@ def spher2cart(r, phi, theta) :
     Transform spherical coordinates into cartesian coordinates
     Input :
     - 0 <= r             (float)
-    - 0 <= phi   <= pi   (float)
-    - 0 <= theta <= 2pi  (float)
+    - 0 <= phi   <= 2pi  (float)
+    - 0 <= theta <=  pi  (float)
 
     Output :
-    - x = r*sin(phi)*cos(theta)  (float)
-    - y = r*sin(phi)*sin(theta)  (float)
-    - z = r*cos(phi)             (float)
+    - x = r*sin(theta)*cos(phi)  (float)
+    - y = r*sin(theta)*sin(phi)  (float)
+    - z = r*cos(theta)           (float)
     """
     return r*np.sin(theta)*np.cos(phi), r*np.sin(theta)*np.sin(phi), r*np.cos(theta)
 
@@ -25,9 +25,11 @@ def cart2spher(x, y, z) :
 
     Output :
     - r     = sqrt(x**2 + y**2 + z**2)  (float)
-    - phi   = arccos(z/r)
-    - theta = 
+    - phi   = arctan(y/x) (+ pi if x < 0)
+    - theta = arccos(z/r)
     """
     r = np.sqrt(x**2 + y**2 + z**2)
-    return r, np.arccos(z/r), np.arctan(y/x)
-
+    if x == 0 : phi = np.pi/2 + np.pi * (y < 0)
+    else : phi = np.arctan(y/x) + np.pi * (x < 0)
+    theta = np.arccos(z/r)
+    return r, phi, theta
