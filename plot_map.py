0eb6d0ad5ae22175b08a947f234fd17ce0da187f
Tavier Aloïs <alois.tavier@student.uclouvain.be>
2024-03-16 12:36:48 +0100

    update plot_map.py

--- plot_map.py
+++ plot_map.py
@@ -65,9 +65,9 @@ def draw_circle_on_sphere(p:float, a:float, radius:float):
     for i in range(6, 600):
         v = asin(radius/(i*1000))
         u = np.mgrid[0:2*np.pi:30j]
-        x1 = (np.sin(v)*np.cos(p)*np.cos(a)*np.cos(u) + np.cos(v)*np.sin(p)*np.cos(a) - np.sin(v)*np.sin(a)*np.sin(u))*(6528.134)
-        y1 = (np.sin(v)*np.cos(p)*np.sin(a)*np.cos(u) + np.cos(v)*np.sin(p)*np.sin(a) + np.sin(v)*np.cos(a)*np.sin(u))*6528.134
-        z1 = -np.sin(v)*np.sin(p)*np.cos(u)*6528.134 + np.cos(v)*np.cos(p)*6528.134
+        x1 = (np.sin(v)*np.cos(p)*np.cos(a)*np.cos(u) + np.cos(v)*np.sin(p)*np.cos(a) - np.sin(v)*np.sin(a)*np.sin(u))*(6428.134)
+        y1 = (np.sin(v)*np.cos(p)*np.sin(a)*np.cos(u) + np.cos(v)*np.sin(p)*np.sin(a) + np.sin(v)*np.cos(a)*np.sin(u))*6428.134
+        z1 = -np.sin(v)*np.sin(p)*np.cos(u)*6428.134 + np.cos(v)*np.cos(p)*6428.134
         x.append(x1) ; y.append(y1) ; z.append(z1)
         
     return x, y, z
@@ -88,12 +88,10 @@ def spherical_coords(lons, lats):
     for lat, lon in zip(lats, lons):
         lat_rad = math.radians(lat)
         lon_rad = math.radians(lon)
-        x = math.cos(lon_rad) * math.cos(lat_rad)*6418.134
-        y = math.cos(lon_rad) * math.sin(lat_rad)*6418.134
-        z = math.sin(lon_rad)*6418.134
-        x_coords.append(x)
-        y_coords.append(y)
-        z_coords.append(z)
+        x = math.cos(lon_rad) * math.cos(lat_rad)*6318.134
+        y = math.cos(lon_rad) * math.sin(lat_rad)*6318.134
+        z = math.sin(lon_rad)*6318.134
+        x_coords.append(x) ; y_coords.append(y) ; z_coords.append(z)
     return x_coords, y_coords, z_coords
 
 def plot_cities(lons, lats):
@@ -107,7 +105,7 @@ def plot_cities(lons, lats):
             None
     '''
     x, y, z = spherical_coords(lats, lons)
-    fig.add_trace(go.Scatter3d(x=x, y=y, z=z, mode='markers', line=dict(color=f'rgb(255, 0,0)', width = 10), showlegend=False ) )
+    fig.add_trace(go.Scatter3d(x=x, y=y, z=z, mode='markers', line=dict(color=f'rgb(190, 0,0)', width = 10), showlegend=False ) )
     
 def plot_satellite(pol, azi, rad):
     '''
@@ -118,11 +116,14 @@ def plot_satellite(pol, azi, rad):
             radius (float): radius of the surface coverd by the satellite
         Returns:
             None
-    '''    
+    '''
+    clor =f'rgb(0, 0, 200)'
     for p, a in zip(pol, azi):
+        x1 = np.array([6350 * math.sin(p) * math.cos(a)])
+        y1 = np.array([6350 * math.sin(p) * math.sin(a)])
+        z1 = np.array([6350 * math.cos(p)])
         x, y, z = draw_circle_on_sphere(p, a, rad)
-        clor =f'rgb(0, 0, 200)'
-
+        fig.add_trace(go.Scatter3d(x=x1, y=y1, z=z1, mode='markers', line=dict(color=f'rgb(0, 0, 100)', width = 4), showlegend=False ) )
         fig.add_surface(z=z, x=x, y=y, colorscale=[[0, clor], [1, clor]],showscale = False, opacity=0.7, showlegend=False, lighting=dict(diffuse=0.1))
 
 def plot_fig(figure):
