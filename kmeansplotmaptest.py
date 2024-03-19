32ff91770b2c211ec284ba74eb4ce2b3e94ebe69
hokkaydo <hokkaydo@users.noreply.github.com>
2024-03-19 08:55:57 +0100

    update

--- kmeansplotmaptest.py
+++ kmeansplotmaptest.py
@@ -14,7 +14,7 @@ cities = []
 for i in range (n_cities) :
     phi = np.random.rand() * 2*np.pi
     theta = np.random.rand() * np.pi
-    cities.append((phi, theta))
+    #cities.append((phi, theta))
     cities.append([np.sin(theta)*np.cos(phi), np.sin(theta)*np.sin(phi), np.cos(theta)])
 cities = r * np.array(cities)
 cities_weights = np.random.randint(1, 100, n_cities)
@@ -25,11 +25,12 @@ satellites_cart = kmeans.spherical_kmeans(cities, [], n_sat)
 def spherical_to_lat_long(data):
     return 180/np.pi * r * data
 
-satellites_polar = conversion.polar(satellites_cart)[1:]
+satellites_polar = np.array(conversion.polar(*satellites_cart.T))[1:]
+cities_polar = np.array(conversion.polar(*cities.T))[1:]
 
 satellites_lat, satellites_long = spherical_to_lat_long(satellites_polar)
-cities_lat, cities_long = spherical_to_lat_long(cities.T)
+cities_lat, cities_long = spherical_to_lat_long(cities_polar)
 
 plot_map.plot_cities(cities_long, cities_lat)
-plot_map.plot_satellite(satellites_lat, satellites_long, 1000)
+plot_map.plot_satellite(satellites_lat, satellites_long, 2000)
 plot_map.plot_fig()
