035825fbdc768f776d956d92dc4f95ae7021cf02
hokkaydo <hokkaydo@users.noreply.github.com>
2024-04-16 10:50:32 +0200

    fix conventions on server'

--- server.py
+++ server.py
@@ -20,22 +20,19 @@ def hello_world():
 def compute():
     request_data = request.get_json()
     print(request_data)
-    N_satellites, cities_coordinates, cities_weights, format, R, H, P, I_necessary, alpha, verbose = request_data.values()
+    N_satellites, cities_gps, cities_weights, format, R, H, P, I_necessary, alpha, verbose = request_data.values()
     N_satellites = int(N_satellites)
-    cities_coordinates = np.array(cities_coordinates).astype(np.float64)
-    cities_spherical = cities_coordinates.copy()
-    cities_spherical.T[0] += 180
-    cities_spherical.T[1] += 90
-    
-    cities_spherical = lat_long_to_spherical(np.array(cities_coordinates).astype(np.float64))
+    cities_gps = np.array(cities_gps).astype(np.float64)
+
+    cities_spherical = gps2spher(np.array(cities_gps).astype(np.float64))
     cities_weights = np.array(cities_weights).astype(np.float64)
     satellites_polar, cost = ssr.spherical_satellites_repartition(int(N_satellites), cities_spherical, cities_weights, format, R, H, P, I_necessary, alpha, verbose)
         
-    satellites_long_lat = spherical_to_lat_long(satellites_polar)
-    print(satellites_long_lat)
+    satellites_gps = spher2gps(satellites_polar)
+    print(satellites_gps)
     plot_map.create_fig()
-    plot_map.plot_cities(cities_coordinates.T[0], cities_coordinates.T[1], cities_weights)
-    plot_map.plot_satellite_long_lat(*satellites_long_lat.T)
+    plot_map.plot_cities(*cities_gps.T, cities_weights)
+    plot_map.plot_satellite_long_lat(*satellites_gps.T)
     filename = "temp_plot.html"
     plot_map.plot_fig(filename, auto_open=False)
     return {'content': os.path.abspath(filename), 'cost': cost()}
